31f5928513288a72292f32d8656613eaf4259840
costajob <costajob@gmail.com>
2018-08-02 14:25:10 +0200

    Mike - fix brittle tests

--- skusclf/tests/classifier_test.py
+++ skusclf/tests/classifier_test.py
@@ -5,22 +5,25 @@ from skusclf import classifier, stubs, training
 
 class TestClassifier(unittest.TestCase):
     def setUp(self):
-        self.ds = training.Dataset(f'{stubs.PATH}/dataset.h5').load()
         filterwarnings('ignore')
+        ds = training.Dataset(f'{stubs.PATH}/dataset.h5', folder=stubs.FOLDER,
+                              persist=False, augmenter=training.Augmenter(.01))
+        ds()
+        self.data = ds.load()
 
     def tearDown(self):
-        self.ds.close()
+        self.data.close()
 
     def test_attributes(self):
-        mod = classifier.Model(self.ds)
+        mod = classifier.Model(self.data)
         self.assertEqual(mod.size, 64) 
         for i in range(0, 3):
             self.assertIn(i, list(mod.y))
 
     def test_prediction(self):
-        mod = classifier.Model(self.ds)
-        res = mod(self.ds['X'][-1])
-        self.assertEqual(res, self.ds['y'][-1].decode('utf-8'))
+        mod = classifier.Model(self.data)
+        res = mod(self.data['X'][-1])
+        self.assertEqual(res, self.data['y'][-1].decode('utf-8'))
 
 
 if __name__ == '__main__':
