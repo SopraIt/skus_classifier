Mike - fixed features scaling

skusclf/training.py
@@ -247,10 +247,11 @@ class Features:
     >>> feat = Features('./my_images')
     '''
     
+    MAX = 1.
+    MIN = 0.
     EXTS = ('jpg', 'jpeg', 'png')
     LIMIT = 0
     BRANDS = ('plain', 'mm', 'gg')
-    MAX_VAL = 255
     FETCHERS = {
         BRANDS[0]: lambda n: path.basename(n).split('.')[0],
         BRANDS[1]: lambda n: path.basename(n).split('-')[0],
@@ -310,10 +311,11 @@ class Features:
         return self.fetcher(name), self._scale(data)
 
     def _scale(self, data):
-        if np.max(data) > 1:
-            data = data / self.MAX_VAL
-        if np.min(data) < 0:
-            data[data < 0] = 0
+        _max = np.max(data)
+        if _max > self.MAX:
+            data = data / _max
+        if np.min(data) < self.MIN:
+            data[data < self.MIN] = self.MIN
         return data
 
     def _augmenting(self, img):
@@ -465,24 +467,20 @@ class DatasetZIP:
     def _entries(self):
         for label, imgs in self.features:
             logger.info('archiving label %s', label)
-            for i, data in enumerate(imgs):
+            for i, img in enumerate(imgs):
                 name = self._filename(i)
                 logger.debug('archiving image %s', name)
-                img = self._img(data, name, label)
+                img = self._img(img, name)
                 arc = self._arc(label, name)
-                if img:
-                    yield(img, arc)
+                yield(img, arc)
 
     def _filename(self, i):
         return f'{self.filename}_{i}.{self.ext}'
 
-    def _img(self, data, name, label):
-        try:
-            _path = path.join(self.dir, name)
-            plt.imsave(_path, data)
-            return _path
-        except ValueError as e:
-            logger.error(f'invalid image data range for {label}: {data.min()} - {data.max()} / {e}')
+    def _img(self, data, name):
+        _path = path.join(self.dir, name)
+        plt.imsave(_path, data)
+        return _path
 
     def _arc(self, label, name):
         return path.join(f'{self.prefix}{label}', name)